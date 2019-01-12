773882150da625988a111d65b60b352222f6873f
Johnny <jzhang@xogrp.com>
2019-01-12 14:05:45 +0800

    Create the generator entry file

---
+++ generators/app/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Generator = require('yeoman-generator');
+const chalk     = require('chalk');
+const Promise   = require('bluebird');
+const fs        = Promise.promisifyAll(require('fs'));
+
+const excludeFolders = ['app','git-hooks', 'set-git-templates', 'lint-and-ignore']
+
+module.exports = class extends Generator {
+    prompting() {
+        return fs.readdirAsync(this._sourceRoot.replace('/app/templates', ''))
+            .then((files) => {
+                this.log(chalk.bold.yellow('Available commands ' + require('../../package.json').version));
+
+                for (let i = 0; i < files.length; i++) {
+                  if(!excludeFolders.includes(files[i])) {
+                    this.log(chalk.bold.green('yo') + ' hapi-generator:' + files[i]);
+                  }
+                }
+            })
+            .catch((error) => {
+                console.error(chalk.red(error));
+                process.exit(1);
+            });
+    }
+};
